8dc83c65affbf87eccd04efd440f296aa357664f
eranda <eranda.g@omobio.net>
2021-03-21 18:27:18 +0530

    routing modification

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,18 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api/staff'], function () use ($router) {
-    $router->get('authors',  ['uses' => 'AuthorController@showAllAuthors']);
-
-    $router->get('authors/{id}', ['uses' => 'AuthorController@showOneAuthor']);
-
-    $router->post('authors', ['uses' => 'AuthorController@create']);
-
-    $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
-
-    $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
-});
-
-// staff related
+// staff related routes
 $router->group(['prefix' => 'api/staff'], function () use ($router) {
     $router->post('add', ['uses' => 'StaffController@add']);
     $router->post('assign', ['uses' => 'StaffController@assign']);
 });                     
 
-// branch related
+// branch related routes
 $router->group(['prefix' => 'api/branch'], function () use ($router) {
     $router->post('add', ['uses' => 'BranchController@add']);
 });
 
-// event related
+// event related routes
 $router->group(['prefix' => 'api/event'], function () use ($router) {
     $router->post('add', ['uses' => 'EventController@add']);
     $router->post('assign/{id}', ['uses' => 'EventController@assign']);
